9c0430a8f07747d03f8771e9fd0d6a626f14c090
wxywr <1261046195@qq.com>
2017-09-09 18:59:46 +0800

    Update answer.js

--- js/answer.js
+++ js/answer.js
@@ -8,5 +8,29 @@ $(function(){
 		$('.face p').html('回答正确');
 		$('.face .img').css('backgroundPosition','0 0');
 	})
+
+	$('.tels').keyup(function(){
+		var phone = $('.tels').val(); 
+		if((/^1[34578]\d{9}$/.test(phone))){
+			$('.submit').css('backgroundPosition','0 0');
+		}
+	})
+
+	$('.submit').click(function(){
+		var phone = $('.tels').val();
+		if(phone==''){
+			alert('请输入您的手机号码！');
+			return false;
+		}
+		else if(!(/^1[34578]\d{9}$/.test(phone))){
+			alert('请输入正确的手机号码!');
+			return false;
+		}
+		else{
+			$('.submit').css('backgroundPosition','0 -0.45rem');
+			$('.submit').val('提交完成');
+			return true;
+		}
+	})
 	
 });
