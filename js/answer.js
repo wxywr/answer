a9b5aaa6f08d2491e47bf4a95663b7e902d77758
wxywr <1261046195@qq.com>
2017-09-10 11:31:24 +0800

    Update answer.js

--- js/answer.js
+++ js/answer.js
@@ -3,10 +3,37 @@ $(window).resize(function(){
 });
 $(function(){
 	$('html').css({'font-size':$(window).width()/3.9+"px"});
+
+	var t = 0;
+	var s = $('.wrap .list').length;
+	function done(){
+		$('.wrap .list').eq(t).show();
+		$('.wrap .list').eq(t).siblings().hide();
+	}
+	$('.button').click(function(){
+		t++;
+		done();
+		if(t>=s-1){
+			$('.button').val('下一页');
+			$('.button').css('backgroundPosition','0 0');
+			$('.button').click(function(){
+				window.location.href = 'contact.html';
+			})
+		}
+	})
+
 	$('.select .true').click(function(){
 		$('.face').css('display','block');
 		$('.face p').html('回答正确');
+		$('.face p').css('color','#009CE5;');
 		$('.face .img').css('backgroundPosition','0 0');
+	})
+
+	$('.select .false').click(function(){
+		$('.face').css('display','block');
+		$('.face p').html('正确选项:C');
+		$('.face p').css('color','#333');
+		$('.face .img').css('backgroundPosition','0 -0.64rem');
 	})
 
 	$('.tels').keyup(function(){
