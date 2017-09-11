8704a621fc874a4955ec95587686caacd8fd5265
wxywr <1261046195@qq.com>
2017-09-11 10:50:14 +0800

    Update answer.js

--- js/answer.js
+++ js/answer.js
@@ -11,6 +11,8 @@ $(function(){
 		$('.wrap .list').eq(n).siblings().hide();
 	}
 	$(".button").click(function(){
+		$('.nextOne .face').css('visibility','hidden');
+		$('.nextOne .button').css('backgroundPosition','0 -0.45rem');
 	 	n++;
 		change();
 		if(n>=s-1){
@@ -22,12 +24,12 @@ $(function(){
 		}
 	})
 
-	$('.topic-a .true').click(function(){
-		$('.topic-a .face').css('visibility','visible');
-		$('.topic-a .face p').html('回答正确');
-		$('.topic-a .face p').css('color','#009CE5;');
-		$('.topic-a .face .img').css('backgroundPosition','0 0');
-		$('.topic-a .button').css('backgroundPosition','0 0');
+	$('.select .true').click(function(){
+		$('.nextOne .face').css('visibility','visible');
+		$('.nextOne .face p').html('回答正确');
+		$('.nextOne .face p').css('color','#009CE5;');
+		$('.nextOne .face .img').css('backgroundPosition','0 0');
+		$('.nextOne .button').css('backgroundPosition','0 0');
 	})
 
 	$('.topic-a .false').click(function(){
@@ -37,6 +39,35 @@ $(function(){
 		$('.topic-a .face .img').css('backgroundPosition','0 -0.64rem');
 		$('.topic-a .button').css('backgroundPosition','0 0');
 	})
+	$('.topic-b .false').click(function(){
+		$('.topic-b .face').css('visibility','visible');
+		$('.topic-b .face p').html('正确选项: <strong>C</strong>');
+		$('.topic-b .face p').css('color','#333');
+		$('.topic-b .face .img').css('backgroundPosition','0 -0.64rem');
+		$('.topic-b .button').css('backgroundPosition','0 0');
+	})
+	$('.topic-c .false').click(function(){
+		$('.topic-c .face').css('visibility','visible');
+		$('.topic-c .face p').html('正确选项: <strong>D</strong>');
+		$('.topic-c .face p').css('color','#333');
+		$('.topic-c .face .img').css('backgroundPosition','0 -0.64rem');
+		$('.topic-c .button').css('backgroundPosition','0 0');
+	})
+	$('.topic-d .false').click(function(){
+		$('.topic-d .face').css('visibility','visible');
+		$('.topic-d .face p').html('正确选项: <strong>D</strong>');
+		$('.topic-d .face p').css('color','#333');
+		$('.topic-d .face .img').css('backgroundPosition','0 -0.64rem');
+		$('.topic-d .button').css('backgroundPosition','0 0');
+	})
+	$('.topic-e .false').click(function(){
+		$('.topic-e .face').css('visibility','visible');
+		$('.topic-e .face p').html('正确选项: <strong>D</strong>');
+		$('.topic-e .face p').css('color','#333');
+		$('.topic-e .face .img').css('backgroundPosition','0 -0.64rem');
+		$('.topic-e .button').css('backgroundPosition','0 0');
+	})
+
 
 	$('.tels').keyup(function(){
 		var phone = $('.tels').val(); 
