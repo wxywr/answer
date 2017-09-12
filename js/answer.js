fa11bd7b6a2f26f91f4580b12ea5cf269bb00333
wxywr <1261046195@qq.com>
2017-09-12 11:03:30 +0800

    Update answer.js

--- js/answer.js
+++ js/answer.js
@@ -76,5 +76,55 @@ $(function(){
         }
 	})
 
+	var dex=4;
+	$('.true').eq(dex).click(function(){
+		$('.face').eq(dex).css('visibility','visible');
+		$('.face p').eq(dex).html('回答正确');
+		$('.face p').eq(dex).addClass('blu');
+		$('.face p').removeClass('bla');
+		$('.face .img').eq(dex).css('backgroundPosition','0 0');
+		$('.button').eq(dex).css('backgroundPosition','0 0');
+	})
+
+	$('.topic-a .false').click(function(){
+		$('.topic-a .face').css('visibility','visible');
+		$('.topic-a .face p').html('正确选项: <strong>D</strong>');
+		$('.topic-a .face p').addClass('bla');
+		$('.nextOne .face p').removeClass('blu');
+		$('.topic-a .face .img').css('backgroundPosition','0 -0.64rem');
+		$('.topic-a .button').css('backgroundPosition','0 0');
+	})
+	$('.topic-b .false').click(function(){
+		$('.topic-b .face').css('visibility','visible');
+		$('.topic-b .face p').html('正确选项: <strong>D</strong>');
+		$('.topic-a .face p').addClass('bla');
+		$('.nextOne .face p').removeClass('blu');
+		$('.topic-b .face .img').css('backgroundPosition','0 -0.64rem');
+		$('.topic-b .button').css('backgroundPosition','0 0');
+	})
+	$('.topic-c .false').click(function(){
+		$('.topic-c .face').css('visibility','visible');
+		$('.topic-c .face p').html('正确选项: <strong>C</strong>');
+		$('.topic-a .face p').addClass('bla');
+		$('.nextOne .face p').removeClass('blu');
+		$('.topic-c .face .img').css('backgroundPosition','0 -0.64rem');
+		$('.topic-c .button').css('backgroundPosition','0 0');
+	})
+	$('.topic-d .false').click(function(){
+		$('.topic-d .face').css('visibility','visible');
+		$('.topic-d .face p').html('正确选项: <strong>D</strong>');
+		$('.topic-a .face p').addClass('bla');
+		$('.nextOne .face p').removeClass('blu');
+		$('.topic-d .face .img').css('backgroundPosition','0 -0.64rem');
+		$('.topic-d .button').css('backgroundPosition','0 0');
+	})
+	$('.topic-e .false').click(function(){
+		$('.topic-e .face').css('visibility','visible');
+		$('.topic-e .face p').html('正确选项: <strong>D</strong>');
+		$('.topic-a .face p').addClass('bla');
+		$('.nextOne .face p').removeClass('blu');
+		$('.topic-e .face .img').css('backgroundPosition','0 -0.64rem');
+		$('.topic-e .button').css('backgroundPosition','0 0');
+	})
 
 });
